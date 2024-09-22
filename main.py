b11dcc501b6d65a440f39d99a74d28cbdd1d3848
Teyllayka <teyylayt@gmail.com>
2024-09-22 10:22:13 +0300

    fix backend

--- main.py
+++ main.py
@@ -17,16 +17,18 @@ from functools import wraps
 from flask_swagger_ui import get_swaggerui_blueprint
 from stripe.checkout import Session
 from flask_mail import Mail, Message
-
+import redis
 
 load_dotenv()
+
+redis_url = os.getenv("REDIS_URL")
+r = redis.from_url(redis_url)
 
 REFRESH_TOKEN_SECRET = os.getenv("REFRESH_TOKEN_SECRET")
 ACCESS_TOKEN_SECRET = os.getenv("ACCESS_TOKEN_SECRET")
 stripe.api_key = os.getenv("STRIPE_SECRET_KEY")
 REFRESH_PASSWORD_SECRET = os.getenv("REFRESH_PASSWORD_SECRET")
 MODE = os.getenv("MODE", "production").lower()
-
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("POSTGRES_URL")
@@ -37,7 +39,6 @@ app.config["MAIL_USE_SSL"] = False
 app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
 app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
 
-
 mail = Mail(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 db = SQLAlchemy(app)
@@ -46,38 +47,29 @@ migrate = Migrate(app, db)
 swagger_ui_blueprint = get_swaggerui_blueprint(
     "/swagger", "/static/swagger.json", config={"app_name": "ZieduVeikals"}
 )
-
 app.register_blueprint(swagger_ui_blueprint, url_prefix="/swagger")
-
 
 class Role(enum.Enum):
     ADMIN = "ADMIN"
     USER = "USER"
 
-
 class Flower(enum.Enum):
     FLOWER = "FLOWER"
     BOUQUET = "BOUQUET"
-
 
 class Status(enum.Enum):
     PENDING = "PENDING"
     COMPLETED = "COMPLETED"
     CANCELLED = "CANCELLED"
 
-
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
     email: Mapped[str]
     password: Mapped[str]
-    refresh_token: Mapped[str] = mapped_column(nullable=True)
     role: Mapped[Role] = mapped_column(Enum(Role, native_enum=True), default=Role.USER)
-    promocode_id: Mapped[int] = mapped_column(
-        db.ForeignKey("promocode.id"), nullable=True
-    )
+    promocode_id: Mapped[int] = mapped_column(db.ForeignKey("promocode.id"), nullable=True)
     current_promocode = db.relationship("Promocode", backref="users")
-
 
 class Product(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -88,23 +80,15 @@ class Product(db.Model):
     description: Mapped[str] = mapped_column(nullable=True)
     type: Mapped[Flower] = mapped_column(Enum(Flower, native_enum=True))
 
-
 class Order(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(db.ForeignKey("user.id"))
-    status: Mapped[Status] = mapped_column(
-        Enum(Status, native_enum=True), default=Status.PENDING
-    )
-    created_at: Mapped[datetime.datetime] = mapped_column(
-        default=datetime.datetime.utcnow
-    )
+    status: Mapped[Status] = mapped_column(Enum(Status, native_enum=True), default=Status.PENDING)
+    created_at: Mapped[datetime.datetime] = mapped_column(default=datetime.datetime.utcnow)
     order_id: Mapped[str] = mapped_column(unique=True)
-    promocode_id: Mapped[int] = mapped_column(
-        db.ForeignKey("promocode.id"), nullable=True
-    )
+    promocode_id: Mapped[int] = mapped_column(db.ForeignKey("promocode.id"), nullable=True)
     promocode = db.relationship("Promocode")
     user = db.relationship("User", backref="orders")
-
 
 class OrderItem(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -115,13 +99,11 @@ class OrderItem(db.Model):
     order = db.relationship("Order", backref="items")
     product = db.relationship("Product")
 
-
 class Promocode(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     code: Mapped[str]
     discount: Mapped[float]
     count_usage: Mapped[int]
-
 
 class CartItem(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -131,15 +113,14 @@ class CartItem(db.Model):
     user = db.relationship("User", backref="cart_items")
     product = db.relationship("Product")
 
-
 def role_required(role):
     def decorator(f):
         @wraps(f)
         def decorated_function(*args, **kwargs):
             token = request.headers.get("Authorization")
             if token:
-                token = token.split(" ")[1]
                 try:
+                    token = token.split(" ")[1]
                     decoded_token = jwt.decode(
                         token, ACCESS_TOKEN_SECRET, algorithms=["HS256"]
                     )
@@ -161,7 +142,6 @@ def role_required(role):
 
     return decorator
 
-
 @app.route("/register", methods=["POST"])
 def register():
     data = request.json
@@ -177,9 +157,7 @@ def register():
     if user:
         return jsonify({"message": "Username already exists"}), 400
 
-    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode(
-        "utf-8"
-    )
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
 
     db.session.add(
         User(username=username, email=email, password=hashed_password, role=Role.USER)
@@ -187,7 +165,6 @@ def register():
     db.session.commit()
 
     return jsonify({"status": "success"}), 201
-
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -210,10 +187,7 @@ def login():
             algorithm="HS256",
         )
 
-        db.session.query(User).filter_by(username=username).update(
-            {"refresh_token": refresh_token}
-        )
-        db.session.commit()
+        r.setex(f"refresh_token:{username}", 3600, refresh_token)
 
         access_token = jwt.encode(
             {
@@ -228,7 +202,6 @@ def login():
     else:
         return jsonify({"message": "Invalid credentials"}), 401
 
-
 @app.route("/refresh", methods=["POST"])
 def refresh():
     data = request.json
@@ -242,43 +215,38 @@ def refresh():
             refresh_token, REFRESH_TOKEN_SECRET, algorithms=["HS256"]
         )
         username = decoded_token["sub"]
-        user = db.session.query(User).filter_by(username=username).first()
 
-        if user and user.refresh_token == refresh_token:
-            access_token = jwt.encode(
-                {
-                    "sub": username,
-                    "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
-                },
-                ACCESS_TOKEN_SECRET,
-                algorithm="HS256",
-            )
+        redis_refresh_token = r.get(f"refresh_token:{username}")
+        if not redis_refresh_token or redis_refresh_token.decode('utf-8') != refresh_token:
+            return jsonify({"message": "Invalid or expired refresh token"}), 401
 
-            refresh_token = jwt.encode(
-                {
-                    "sub": username,
-                    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
-                },
-                REFRESH_TOKEN_SECRET,
-                algorithm="HS256",
-            )
+        access_token = jwt.encode(
+            {
+                "sub": username,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=15),
+            },
+            ACCESS_TOKEN_SECRET,
+            algorithm="HS256",
+        )
 
-            db.session.query(User).filter_by(username=username).update(
-                {"refresh_token": refresh_token}
-            )
+        new_refresh_token = jwt.encode(
+            {
+                "sub": username,
+                "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+            },
+            REFRESH_TOKEN_SECRET,
+            algorithm="HS256",
+        )
 
-            db.session.commit()
+        r.setex(f"refresh_token:{username}", 3600, new_refresh_token)
 
-            return jsonify(
-                {"access_token": access_token, "refresh_token": refresh_token}
-            )
-        else:
-            return jsonify({"message": "Invalid refresh token"}), 401
+        return jsonify(
+            {"access_token": access_token, "refresh_token": new_refresh_token}
+        )
     except jwt.ExpiredSignatureError:
-        return jsonify({"message": "Invalid refresh token"}), 401
+        return jsonify({"message": "Expired refresh token"}), 401
     except jwt.InvalidTokenError:
         return jsonify({"message": "Invalid refresh token"}), 401
-
 
 @app.route("/change_password", methods=["PATCH"])
 @role_required(Role.USER)
@@ -308,7 +276,6 @@ def change_password(user):
     else:
         return jsonify({"message": "Invalid old password"}), 400
 
-
 @app.route("/reset-password", methods=["POST"])
 def reset_password():
     data = request.json
@@ -333,10 +300,10 @@ def reset_password():
     )
 
     msg = Message(
-        "Hello",
+        "Password Reset",
         sender="from@example.com",
-        recipients=["to@example.com"],
-        body=f"This is a test email sent from Flask-Mail! {refresh}",
+        recipients=[user.email],
+        body=f"Click the link to reset your password: {refresh}",
     )
 
     mail.send(msg)
@@ -346,11 +313,10 @@ def reset_password():
         200,
     )
 
-
 @app.route("/reset", methods=["GET", "POST"])
 def reset():
     if request.method == "GET":
-        data = request.json
+        data = request.args 
         token = data.get("token")
 
         if not token:
@@ -404,7 +370,6 @@ def reset():
         except:
             return jsonify({"message": "Invalid token"}), 400
 
-
 @app.route("/product/<int:id>", methods=["GET", "PATCH", "DELETE"])
 def handle_product(id):
     product = db.session.query(Product).filter_by(id=id).first()
@@ -425,7 +390,6 @@ def handle_product(id):
 
     return modify_or_delete_product(product)
 
-
 @role_required(Role.ADMIN)
 def modify_or_delete_product(user, product):
     if request.method == "PATCH":
@@ -442,7 +406,6 @@ def modify_or_delete_product(user, product):
         db.session.delete(product)
         db.session.commit()
         return jsonify({"status": "success", "message": "Product deleted"}), 200
-
 
 @app.route("/products", methods=["GET", "POST"])
 def products():
@@ -469,7 +432,6 @@ def products():
 
     return create_product()
 
-
 @role_required(Role.ADMIN)
 def create_product(current_user: User):
     if current_user.role != Role.ADMIN:
@@ -495,7 +457,6 @@ def create_product(current_user: User):
     db.session.add(product)
     db.session.commit()
     return jsonify({"status": "success"}), 201
-
 
 @app.route("/get_users", methods=["GET"])
 @role_required(Role.ADMIN)
@@ -525,7 +486,6 @@ def get_users(user):
         )
     return jsonify({"users": user_list})
 
-
 def filter_products(products):
     product_ids = [product["id"] for product in products]
     db_products = db.session.query(Product).filter(Product.id.in_(product_ids)).all()
@@ -548,7 +508,6 @@ def filter_products(products):
             products.remove(product)
 
     return {"products": products, "total_price": total_price}
-
 
 @app.route("/add", methods=["POST"])
 @role_required(Role.USER)
@@ -593,7 +552,6 @@ def add(user):
         ),
         201,
     )
-
 
 @app.route("/cart", methods=["GET", "DELETE"])
 @role_required(Role.USER)
@@ -653,21 +611,20 @@ def view_cart(user):
 
         return jsonify({"message": "Cart cleared successfully"}), 200
 
-
 @app.route("/cart/<int:id>", methods=["PATCH", "DELETE"])
 @role_required(Role.USER)
 def modify_or_delete_cart_item(user, id):
     cart_item = db.session.query(CartItem).filter_by(id=id, user_id=user.id).first()
-    product = db.session.query(Product).filter_by(id=cart_item.product_id).first()
-
     if not cart_item:
         return jsonify({"message": "Cart item not found"}), 404
+
+    product = db.session.query(Product).filter_by(id=cart_item.product_id).first()
 
     if request.method == "PATCH":
         data = request.json
         new_quantity = data.get("quantity")
 
-        if new_quantity is None or new_quantity < 1 or product.quantity < new_quantity:
+        if new_quantity is None or new_quantity < 1 or (product and product.quantity < new_quantity):
             return jsonify({"message": "Invalid quantity"}), 400
 
         cart_item.quantity = new_quantity
@@ -680,7 +637,6 @@ def modify_or_delete_cart_item(user, id):
         db.session.commit()
 
         return jsonify({"message": "Cart item deleted successfully"}), 200
-
 
 def count_total_price(cart_items):
     ids = [item.product_id for item in cart_items]
@@ -700,27 +656,24 @@ def count_total_price(cart_items):
             new_item["price"] = product.price
             new_item["name"] = product.name
             new_item["description"] = product.description
-            new_item["images"] = [product.photo]
-            total_price += item.quantity * product.price
+            new_item["images"] = [product.photo] if product.photo else []
+            total_price += new_item["quantity"] * product.price
             new_cart_items.append(new_item)
 
     return {"total_price": total_price, "products": new_cart_items}
 
-
-current_time = int(time.time())
-
-
 def create_payment_link(filtered, promocode) -> Session:
     line_items = []
     for product in filtered["products"]:
+        unit_amount = int(round(product["price"] * 100)) 
+        if promocode:
+            unit_amount = round(unit_amount * (1 - promocode.discount / 100))
+            unit_amount = int(unit_amount)
+
         line_item = {
             "price_data": {
                 "currency": "eur",
-                "unit_amount": (
-                    round(int(product["price"] * 100) * (1 - promocode.discount / 100))
-                    if promocode
-                    else int(round(product["price"] * 100))
-                ),
+                "unit_amount": unit_amount,
                 "product_data": {
                     "name": product["name"],
                     "description": product["description"],
@@ -735,13 +688,12 @@ def create_payment_link(filtered, promocode) -> Session:
         payment_method_types=["card"],
         line_items=line_items,
         mode="payment",
-        success_url=f"https://youtube.com",
-        cancel_url=f"https://youtube.coooom",
+        success_url=f"https://yourdomain.com/success",
+        cancel_url=f"https://yourdomain.com/cancel",
         client_reference_id=str(uuid.uuid4()),
-        expires_at=current_time + 2000,
+        expires_at=int(time.time()) + 2000,
     )
     return checkout_session
-
 
 @app.route("/buy", methods=["POST"])
 @role_required(Role.USER)
@@ -789,16 +741,12 @@ def buy(user):
                     order_id=new_order.id,
                     product_id=product["product_id"],
                     quantity=product["quantity"],
-                    price=db.session.query(Product)
-                    .filter_by(id=product["product_id"])
-                    .first()
-                    .price,
+                    price=db_product.price,
                 )
             )
     db.session.commit()
 
     return jsonify({"payment_link": res.url}), 201
-
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
@@ -839,7 +787,6 @@ def webhook():
 
     return {"status": "error", "message": "Unhandled event type."}, 400
 
-
 @app.route("/orders", methods=["GET"])
 @role_required(Role.USER)
 def get_orders(user):
@@ -871,7 +818,7 @@ def get_orders(user):
                 "email": order.user.email,
                 "role": order.user.role.value,
             },
-            "promocode": promocode_data,  # Add promocode data here
+            "promocode": promocode_data, 
             "items": [
                 {
                     "id": item.id,
@@ -886,7 +833,6 @@ def get_orders(user):
         order_list.append(order_data)
 
     return jsonify({"orders": order_list}), 200
-
 
 @app.route("/promocodes", methods=["POST", "GET"])
 @role_required(Role.ADMIN)
@@ -928,7 +874,6 @@ def promocodes(current_user):
         db.session.commit()
         return jsonify({"status": "success"}), 201
 
-
 @app.route("/promocode/<string:id>", methods=["PATCH", "DELETE", "POST"])
 @role_required(Role.USER)
 def handle_promocode(user, id):
@@ -942,7 +887,6 @@ def handle_promocode(user, id):
         return jsonify({"message": "Promocode not found"}), 404
 
     if request.method == "POST":
-
         if user.current_promocode:
             user.current_promocode.count_usage += 1
 
@@ -952,7 +896,6 @@ def handle_promocode(user, id):
         return jsonify({"status": "success", "message": "Promocode applied"}), 200
 
     return edit_delete_promocode(promocode, user)
-
 
 def edit_delete_promocode(promocode, user):
     if user.role != Role.ADMIN:
@@ -970,7 +913,6 @@ def edit_delete_promocode(promocode, user):
         db.session.delete(promocode)
         db.session.commit()
         return jsonify({"status": "success", "message": "Promocode deleted"}), 200
-
 
 if MODE == "dev":
 
@@ -994,7 +936,6 @@ if MODE == "dev":
         )
 
     print("Development mode enabled: /make_admin route is available.")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
